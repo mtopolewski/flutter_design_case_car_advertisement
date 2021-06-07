b5b5c5a80f0c39fb22446a7a45e03ae407029fec
Mariusz Topolewski <mariusz.topolewski@leaware.com>
2021-06-07 15:39:41 +0200

    Frontend - work in progress

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -18,25 +18,25 @@ class HomePage extends StatelessWidget {
                       child: Stack(
                         children: [
                           Padding(
-                            padding: const EdgeInsets.fromLTRB(40, 0, 40, 20),
+                            padding: const EdgeInsets.fromLTRB(40, 0, 40, 22),
                             child: ClipPath(
-                              clipper: Thirdlipper(),
+                              clipper: FirstClipper(),
                               child: Container(
                                 decoration: BoxDecoration(
-                                    color: Color(0xFFB41A47),
+                                    color: Color(0xFFb81a48), //B41A47 //bd1c4b
                                     borderRadius:
                                         BorderRadius.all(Radius.circular(48))),
                               ),
                             ),
                           ),
                           Padding(
-                            padding: const EdgeInsets.fromLTRB(20, 0, 20, 30),
+                            padding: const EdgeInsets.fromLTRB(20, 0, 20, 32),
                             child: ClipPath(
-                              clipper: Thirdlipper(),
+                              clipper: SecondClipper(),
                               child: Container(
                                 decoration: BoxDecoration(
-                                    color: Color(0xFFCE235B),
-                                    //border: Border.all(color: Colors.black),
+                                    color: Color(
+                                        0xFFdb2c66), //0xFFCE235B //0x0FFC11A52
                                     borderRadius:
                                         BorderRadius.all(Radius.circular(48))),
                               ),
@@ -45,7 +45,6 @@ class HomePage extends StatelessWidget {
                           Padding(
                             padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
                             child: AspectRatio(
-                              //padding: const EdgeInsets.fromLTRB(0, 0, 0, 20),
                               aspectRatio: 1,
                               child: Container(
                                 decoration: BoxDecoration(
@@ -61,7 +60,7 @@ class HomePage extends StatelessWidget {
                                       BorderRadius.all(Radius.circular(48)),
                                 ),
                                 child: ClipPath(
-                                  clipper: Thirdlipper(),
+                                  clipper: ThirdClipper(),
                                   child: Container(
                                     decoration: BoxDecoration(
                                       color: Colors.white,
@@ -82,7 +81,7 @@ class HomePage extends StatelessWidget {
   }
 }
 
-class Thirdlipper extends CustomClipper<Path> {
+class ThirdClipper extends CustomClipper<Path> {
   @override
   Path getClip(Size size) {
     var path = Path();
@@ -113,9 +112,6 @@ class Thirdlipper extends CustomClipper<Path> {
     path.quadraticBezierTo(thirdControlPoint.dx, thirdControlPoint.dy,
         thirdEndPoint.dx, thirdEndPoint.dy);
 
-    //path.lineTo(size.width / 2, size.height * 0.9);
-    //path.lineTo(size.width * 0.68, size.height);
-
     path.lineTo(size.width, size.height);
     path.lineTo(size.width, 0);
     path.close();
@@ -133,30 +129,30 @@ class SecondClipper extends CustomClipper<Path> {
     var path = Path();
     path.lineTo(0, size.height);
 
-    path.lineTo(size.width * 0.3, size.height);
+    path.lineTo(size.width * 0.32, size.height);
 
     var offsetX = 20;
 
-    var firstControlPoint = Offset(size.width * 0.4, size.height * 1);
-    var firstEndPoint = Offset(size.width / 2 - offsetX, size.height * 0.95);
+    var fsepY = size.height * 0.95;
+    var fcpX = size.width * 0.4;
+
+    var firstControlPoint = Offset(fcpX, size.height);
+    var firstEndPoint = Offset(size.width / 2 - offsetX, fsepY);
 
     path.quadraticBezierTo(firstControlPoint.dx, firstControlPoint.dy,
         firstEndPoint.dx, firstEndPoint.dy);
 
-    var secondControlPoint = Offset(size.width / 2, size.height * 0.9);
-    var secondEndPoint = Offset(size.width / 2 + offsetX, size.height * 0.95);
+    var secondControlPoint = Offset(size.width / 2, size.height * 0.895);
+    var secondEndPoint = Offset(size.width / 2 + offsetX, fsepY);
 
     path.quadraticBezierTo(secondControlPoint.dx, secondControlPoint.dy,
         secondEndPoint.dx, secondEndPoint.dy);
 
-    var thirdControlPoint = Offset(size.width * 0.6, size.height * 1);
-    var thirdEndPoint = Offset(size.width * 0.7, size.height);
+    var thirdControlPoint = Offset(size.width - fcpX, size.height);
+    var thirdEndPoint = Offset(size.width * 0.68, size.height);
 
     path.quadraticBezierTo(thirdControlPoint.dx, thirdControlPoint.dy,
         thirdEndPoint.dx, thirdEndPoint.dy);
-
-    //path.lineTo(size.width / 2, size.height * 0.9);
-    path.lineTo(size.width * 0.7, size.height);
 
     path.lineTo(size.width, size.height);
     path.lineTo(size.width, 0);
@@ -173,33 +169,32 @@ class FirstClipper extends CustomClipper<Path> {
   @override
   Path getClip(Size size) {
     var path = Path();
-    double degToRad(num deg) => deg * (Math.pi / 180.0);
-
     path.lineTo(0, size.height);
-    path.lineTo(size.width * 0.25, size.height);
 
-    // var firstStart = Offset(size.width * 0.25, size.height * 0.99999);
-    // var firstEnd = Offset(size.width * 0.40, size.height * 0.92);
+    path.lineTo(size.width * 0.32, size.height);
 
-    // path.quadraticBezierTo(
-    //     firstStart.dx, firstStart.dy, firstEnd.dx, firstEnd.dy);
-    path.quadraticBezierTo(size.width * 0.35, size.height * 1,
-        size.width * 0.42, size.height * 0.92);
+    var offsetX = 17;
 
-    path.quadraticBezierTo(size.width * 0.5, size.height * 0.84,
-        size.width * 0.58, size.height * 0.92);
+    var fsepY = size.height * 0.95;
+    var fcpX = size.width * 0.4;
 
-    path.quadraticBezierTo(size.width * 0.65, size.height * 1.0,
-        size.width * 0.74, size.height * 1);
+    var firstControlPoint = Offset(fcpX, size.height);
+    var firstEndPoint = Offset(size.width / 2 - offsetX, fsepY);
 
-    // Offset center = Offset(size.width / 2, size.height);
-    // path.arcTo(
-    //     // Rect.fromLTWH(
-    //     //     size.width / 2, size.height / 2, size.width / 4, size.height / 4),
-    //     Rect.fromCircle(center: center, radius: 2),
-    //     degToRad(0),
-    //     degToRad(90),
-    //     true);
+    path.quadraticBezierTo(firstControlPoint.dx, firstControlPoint.dy,
+        firstEndPoint.dx, firstEndPoint.dy);
+
+    var secondControlPoint = Offset(size.width / 2, size.height * 0.91);
+    var secondEndPoint = Offset(size.width / 2 + offsetX, fsepY);
+
+    path.quadraticBezierTo(secondControlPoint.dx, secondControlPoint.dy,
+        secondEndPoint.dx, secondEndPoint.dy);
+
+    var thirdControlPoint = Offset(size.width - fcpX, size.height);
+    var thirdEndPoint = Offset(size.width * 0.68, size.height);
+
+    path.quadraticBezierTo(thirdControlPoint.dx, thirdControlPoint.dy,
+        thirdEndPoint.dx, thirdEndPoint.dy);
 
     path.lineTo(size.width, size.height);
     path.lineTo(size.width, 0);
@@ -208,82 +203,8 @@ class FirstClipper extends CustomClipper<Path> {
     return path;
   }
 
-  // @override
-  // Path getClip(Size size) {
-  //   var path = Path();
-
-  //   path.moveTo(size.width * 0.15, size.height);
-
-  //   path.quadraticBezierTo(size.width * 0.25, size.height * 0.99999,
-  //       size.width * 0.40, size.height * 0.92);
-
-  //   path.quadraticBezierTo(size.width * 0.5, size.height * 0.87,
-  //       size.width * 0.60, size.height * 0.92);
-
-  //   path.quadraticBezierTo(size.width * 0.75, size.height * 0.99999,
-  //       size.width * 0.85, size.height);
-
-  //   path.lineTo(size.width, size.height);
-  //   path.close();
-
-  //   return path;
-  // }
-
-  // @override
-  // getClip(Size size) {
-  //   var g1 = 40;
-  //   var d1 = 2 * g1;
-  //   var g2 = 1 * g1;
-  //   var curvePositionY = 6 / 8;
-  //   var path = new Path();
-  //   path.lineTo(0, curvePositionY * size.height);
-
-  //   var firstControlPoint =
-  //       Offset(size.width / 6, curvePositionY * size.height - g1);
-  //   var firstEndPoint = Offset(size.width / 3, curvePositionY * size.height);
-  //   var secondControlPoint =
-  //       Offset(3 * size.width / 4, curvePositionY * size.height + d1);
-  //   var secondEndPoint = Offset(size.width, curvePositionY * size.height - g2);
-
-  //   path.quadraticBezierTo(firstControlPoint.dx, firstControlPoint.dy,
-  //       firstEndPoint.dx, firstEndPoint.dy);
-  //   path.quadraticBezierTo(secondControlPoint.dx, secondControlPoint.dy,
-  //       secondEndPoint.dx, secondEndPoint.dy);
-
-  //   path.lineTo(size.width, curvePositionY * size.height);
-  //   path.lineTo(size.width, size.height);
-  //   path.lineTo(0, size.height);
-
-  //   path.close();
-  //   return path;
-  // }
-
   @override
   bool shouldReclip(CustomClipper oldClipper) {
     return true;
   }
-
-  // @override
-  // Path getClip(Size size) {
-  //   final double innerCircleRadius = -50.0;
-
-  //   Path path = Path();
-  //   path.lineTo(0, size.height);
-  //   path.quadraticBezierTo(size.width / 2 - (innerCircleRadius / 2) - 30,
-  //       size.height + 15, size.width / 2 - 75, size.height + 50);
-  //   path.cubicTo(
-  //       size.width / 2 - 40,
-  //       size.height + innerCircleRadius - 40,
-  //       size.width / 2 + 40,
-  //       size.height + innerCircleRadius - 40,
-  //       size.width / 2 + 75,
-  //       size.height + 50);
-  //   path.quadraticBezierTo(size.width / 2 + (innerCircleRadius / 2) + 30,
-  //       size.height + 15, size.width, size.height);
-  //   path.lineTo(size.width, 0.0);
-  //   path.close();
-
-  //   return path;
-  //   ;
-  // }
 }
